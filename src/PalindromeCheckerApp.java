b3d329a4271032390ee077043af619cef66e0cec
Varshith2568 <varshithreddyr.2007@gmail.com>
2026-02-20 10:22:14 +0530

    UC3: Palindrome Check Using String Reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -19,6 +19,20 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println(input + " is NOT a Palindrome");
         }
+
+        String input = "madam";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+
     }
 }
 
